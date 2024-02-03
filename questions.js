d9ea4831f720c4a65894f65ca202f254dd147233
Melissa Ruelas <melissaruelas01@outlook.com>
2024-02-03 14:31:04 -0600

    first commit

---
+++ questions.js
@@ -0,0 +1,57 @@
+let questions = [
+    {
+        numb: 1,
+        question: "What spell did Harry use to kill Lord Voldemort?",
+        answer: "A. Expelliarmus",
+        options: [
+            "A. Expelliarmus",
+            "B. Expecto Patronum",
+            "C. Avada Kedavra",
+            "D. Accio"
+        ]
+    },
+    {
+        numb: 2,
+        question: "At the first meeting of the Duelling Club, Draco Malfoy summoned what animal with the spell ‘Serpensortia’?",
+        answer: "B. Snake",
+        options: [
+            "A. Frog",
+            "B. Snake",
+            "C. Dragon",
+            "D. Bear"
+        ]
+    },
+    {
+        numb: 3,
+        question: "Which Patronus belongs to Luna Lovegood?",
+        answer: "B. Rabbit",
+        options: [
+            "A. Doe",
+            "B. Rabbit",
+            "C. Dog",
+            "D. Horse"
+        ]
+    },
+    {
+        numb: 4,
+        question: "Who guards the entrance to the Gryffindor common room?",
+        answer: "D. The Fat Lady",
+        options: [
+            "A. The Grey Lady",
+            "B. The Fat Friar",
+            "C. The Bloody Baron",
+            "D. The Fat Lady"
+        ]
+    },
+    {
+        numb: 5,
+        question: "Who has given Harry Potter the Invisibility cloak?",
+        answer: "A. Dumbledore",
+        options: [
+            "A. Dumbledore",
+            "B. Mad-eye Moody",
+            "C. Professor Snape",
+            "D. Dobby"
+        ]
+    }
+];
